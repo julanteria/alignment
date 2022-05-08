9bdb30d19dc40d365302be640f396ea7cf205ecd
julanteria <j.vorpahl@live.de>
2022-05-08 14:47:17 +0200

    implemented getSemiglobalCostmatrix

--- doAlign.py
+++ doAlign.py
@@ -5,14 +5,14 @@ import sys
 def main():
     string1 = sys.argv[1]
     string2 = sys.argv[2]
-    insCost = int(sys.argv[3])
-    delCost = int(sys.argv[4])
-    misCost = int(sys.argv[5])
-    matchCost = int(sys.argv[6])
+    matchCost = int(sys.argv[3])
+    insCost = int(sys.argv[4])
+    delCost = int(sys.argv[5])
+    misCost = int(sys.argv[6])
     aligmentType = str(sys.argv[7])
 
     alignment1 = alignment(string1, string2, matchCost, insCost, delCost, misCost, aligmentType)
-    print(alignment1.Costmatrix)
+    print(alignment1.semiglobalCostmatrix)
 
 
 if __name__ == '__main__':
--- oop.py
+++ oop.py
@@ -12,10 +12,23 @@ class alignment:
         self.deleteCost = d
         self.missCost = mi
         self.aligmentType = aligmentType
-        self.Costmatrix = []
+        self.globalCostmatrix = []
+        self.localCostmatrix = []
+        self.semiglobalCostmatrix = []
 
         if aligmentType == "g":
-            self.Costmatrix = self.getGlobalCostMatrix()
+            self.globalCostmatrix = self.getGlobalCostMatrix()
+
+        if aligmentType == "s":
+            self.semiglobalCostmatrix = self.getSemiglobalCostmatrix()
+
+        if aligmentType == "l":
+            self.localCostmatrix = self.getLocalCostMatrix()
+
+        print(self.matchCost)
+        print(self.insertCost)
+        print(self.deleteCost)
+        print(self.missCost)
 
 
         #elif self.alignment == "l":
@@ -31,6 +44,20 @@ class alignment:
 
         if ch1 != ch2:
             return self.missCost
+
+
+    def ScoreFunction(self,str1,str2,i,j):
+
+        ch1 = str1[i-1]
+        ch2 = str2[j-1]
+
+        if ch1 == ch2:
+            return self.matchCost
+
+
+        if ch1 != ch2:
+            return self.missCost
+
 
 
     def getGlobalCostMatrix(self):
@@ -58,6 +85,95 @@ class alignment:
 
 
         return D
+
+    def getSemiglobalCostmatrix(self):
+        l1 = len(self.string1)+1
+        l2 = len(self.string2)+1
+
+        #initializes Numpy Matrix with zeros
+        D = np.zeros(shape=(l1,l2)).astype('int')
+
+
+        #initializes first row
+        for j in range(1,l2):
+            X = D[0][j-1] + self.insertCost
+            D[0][j] = X
+
+
+        #initializes first column
+        #only zeros because of semi-global
+        for i in range(1,l1):
+            D[i][0] = 0
+
+
+        #loops through all matrix entrys except D[0][0]
+        #fills all entrtys according to alignment rules and custom Insertion-, Deletion- and Missmatch-Cost
+        #calls function CostFunction(str1,str2,i,j)
+        out = ""
+        for i in range(1,l1):
+
+            for j in range(1,l2):
+
+
+                #zero cost if we are at the end of str2 because of semi-global
+                if j == len(self.string2):
+                    down = D[i-1][j] + 0
+
+                else:
+                    down = D[i-1][j] + self.insertCost
+
+
+                right = D[i][j-1] + self.deleteCost
+
+
+                diag = D[i-1][j-1] + self.costFunction(self.string1,self.string2,i,j)
+
+                D[i][j] = min(right,down,diag)
+
+        return D
+
+    #wtf not working 
+    def getLocalCostMatrix(self):
+        l1 = len(self.string1)+1
+        l2 = len(self.string2)+1
+        D = np.zeros(shape=(l1,l2)).astype('int')
+
+        for i in range(1,l2):
+            D[0][i] = 0 
+
+        for j in range(1,l1):
+            D[j][0] = 0
+
+
+        for i in range(1,l1):
+            for j in range(1,l2):
+
+                right = D[i][j-1] - self.deleteCost
+
+                if right < 0:
+                    right = 0
+
+                down = D[i-1][j] - self.insertCost
+
+                if down < 0:
+                    down = 0
+
+                diag = D[i-1][j-1] + self.ScoreFunction(self.string1,self.string2,i,j)
+
+                if diag < 0:
+                    diag = 0
+
+
+
+
+                D[i][j] = max(right,down,diag)
+
+        return D
+
+
+
+
+
 
 
 
